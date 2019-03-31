96c73754dbc1de697f322a682f9aaa7f9cd8b1d2
Simon Quigley <tsimonq2@ubuntu.com>
2019-03-30 20:06:00 -0500

    Ensure pkg_name is defined.

--- lugito/webhooks.py
+++ lugito/webhooks.py
@@ -178,6 +178,7 @@ def jenkinstrigger():
             return 'Ok'
 
         object_type = lugito.request_data["object"]["type"]
+        pkg_name = lugito.get_object_string("name")
 
         if object_type == "CMIT":
             logger.debug("Object is a commit.")
